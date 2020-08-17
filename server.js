224679d9cc3e5dfaac3567f8c4de4cd5c9482725
Rami <ramiayoub12123@gmail.com>
2020-08-17 13:35:41 +0300

    aaa

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express();
+
+app.use(express.static('public'))
+
+const port = process.env.PORT || 4000;
+app.listen(port,()=>{console.log('server listen on port ',port)})
