d06d2ecb0468807509275f758425319e72b312cb
Rami <ramiayoub12123@gmail.com>
2020-08-17 13:58:31 +0300

    Up

---
+++ family/public/precache-manifest.0c9895fef17c5344a8052b15bdc7b563.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ccbc8309abf7a097914f2a7e2e8d77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd23ca2f39ac5a8a89fc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "035f3d347e08667f8e26",
+    "url": "/static/js/2.7dc9c7c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7dc9c7c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd23ca2f39ac5a8a89fc",
+    "url": "/static/js/main.3b5f7713.chunk.js"
+  },
+  {
+    "revision": "2272923afc6c5ac703f7",
+    "url": "/static/js/runtime-main.823863db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
