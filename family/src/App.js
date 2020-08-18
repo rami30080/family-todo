47be3054ef814b067f432bb7cbd70f9f0b25a3f9
Nivin <nivin.abbas1@gmail.com>
2020-08-18 10:00:36 +0300

    updates

--- family/src/App.js
+++ family/src/App.js
@@ -1,67 +1,54 @@
-import React ,{useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './view/login/Login';
-import Register from './view/register/Register';
-import Main from './view/main/Main';
+import React from "react";
+import "./App.css";
+import Login from "./view/login/Login";
+import Register from "./view/register/Register";
+import Main from "./view/main/Main";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useHistory
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
-    <div className="App">
-
-      <Switch>
-      <Route path="/main">
-            <Logged/>
+      <div className="App">
+        <Switch>
+          <Route path="/main">
+            <Logged />
           </Route>
-      <Route path="/register">
-            <Reg/>
+          <Route path="/register">
+            <Reg />
           </Route>
           <Route path="/">
-            <Home/>
+            <Home />
           </Route>
-          
         </Switch>
-    </div>
+      </div>
     </Router>
   );
 }
 
-function Home(){
-  return(
-    <div className='page'>
-      <Login/>
+function Home() {
+  return (
+    <div className="page">
+      <Login />
       <Link to="/register">Register</Link>
-
     </div>
-  )
+  );
 }
 
-function Reg(){
-  return(
-    <div className='page'>
-      <Register/>
-      
+function Reg() {
+  return (
+    <div className="page">
+      <Register />
     </div>
-  )
+  );
 }
 
-function Logged(){
-  return(
-    <div className='page'>
-      <Main/>
+function Logged() {
+  return (
+    <div className="page">
+      <Main />
     </div>
-  )
+  );
 }
 
-function Task(){
-  
-}
+// function Task() {}
--- family/src/view/register/Register.js
+++ family/src/view/register/Register.js
@@ -1,49 +1,49 @@
-import React from 'react'
-import './Register.css'
+import React from "react";
+import "./Register.css";
 
-const Register = ()=>{
+const Register = () => {
+  function registerHandler(e) {
+    e.preventDefault();
+    const { userEmail, Name, Family, Img, Password } = e.target.elements;
+    const name = Name.value;
+    const family = Family.value;
+    const img = Img.value;
+    const pass = Password.value;
+    const useremail = userEmail.value;
+    fetch("/register", {
+      method: "POST",
+      body: JSON.stringify({ useremail, name, family, img, pass }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const { register } = data;
+        console.log(register);
+      });
+  }
 
-    function registerHandler(e){
-        e.preventDefault();
-        const {userEmail ,Name,Family,Img,Password} = e.target.elements
-        const name = Name.value
-        const family = Family.value
-        const img = Img.value
-        const pass = Password.value
-        const useremail = userEmail.value
-        fetch('/register', {
-            method: 'POST',
-            body: JSON.stringify({ useremail ,name, family , img , pass }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(res => res.json())
-            .then(data => {
-                const {register} = data
-                console.log(register)
+  return (
+    <div className="body">
+      <form className="container" onSubmit={registerHandler}>
+        <br></br>
+        <input type="text" placeholder="userEmail" name="userEmail"></input>
+        <br></br>
+        <input type="text" placeholder="name" name="Name"></input>
+        <br></br>
+        <input type="text" placeholder="family" name="Family"></input>
+        <br></br>
+        <input type="text" placeholder="image" name="Img"></input>
+        <br></br>
+        <input type="password" placeholder="password" name="Password"></input>
+        <br></br>
+        <button className="button" type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
 
-            })
-        
-    }
-
-
-
-    return(
-        <div>
-            <form onSubmit={registerHandler}>
-            <input type="text" placeholder="userEmail" name="userEmail" ></input>
-            <input type="text" placeholder="name" name="Name" ></input>
-            <input type="text" placeholder="family" name="Family" ></input>
-            <input type="text" placeholder="image" name="Img" ></input>
-            <input type="password" placeholder="password" name="Password" ></input>
-            <button type='submit'>Login</button>
-            </form>
-        </div>
-    )
-
-}
-
-
-
-export default Register
-
+export default Register;
