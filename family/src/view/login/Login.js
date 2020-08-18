f2e6bc8b69033d116bd64821c8ea4b96f1a5ec00
Nivin <nivin.abbas1@gmail.com>
2020-08-18 12:01:23 +0300

    combine UI with Backend

--- family/src/view/login/Login.js
+++ family/src/view/login/Login.js
@@ -1,51 +1,65 @@
-import React from 'react'
-import './Login.css'
-import { Redirect} from 'react-router';
-import {useHistory }from "react-router-dom";
+import { Redirect } from "react-router";
+import { useHistory } from "react-router-dom";
+import React from "react";
+import "./Login.css";
 
-const Login = ()=>{
-    let history=useHistory()
-    function loginHandler(e){
-        e.preventDefault();
-        const {Name,Family,Password} = e.target.elements
-        const name = Name.value
-        const family = Family.value
-        const pass = Password.value
-        fetch('/login', {
-            method: 'POST',
-            body: JSON.stringify({ name, family , pass }),
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(res => res.json())
-            .then(data => {
-                const {loggedin} = data
-                if(loggedin){
-                    localStorage.setItem('familyID', family);
-                    localStorage.setItem('NameID', name);
-                    history.push('/main')
+const Login = () => {
+  let history = useHistory();
+  function loginHandler(e) {
+    e.preventDefault();
+    const { Name, Family, Password } = e.target.elements;
+    const name = Name.value;
+    const family = Family.value;
+    const pass = Password.value;
+    fetch("/login", {
+      method: "POST",
+      body: JSON.stringify({ name, family, pass }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const { loggedin } = data;
+        if (loggedin) {
+          localStorage.setItem("familyID", family);
+          localStorage.setItem("NameID", name);
+          history.push("/main");
+        } else {
+          document.getElementById("badmsg").innerText = "User Not Exist's";
+        }
+      });
+  }
+  return (
+    <div className="body">
+      <p className="container">
+        <h1 className="login">Log in</h1>
+        <form onSubmit={loginHandler}>
+          <input
+            className="un"
+            type="text"
+            placeholder="First Name"
+            name="Name"
+          ></input>
+          <input
+            className="un"
+            type="text"
+            placeholder="Last Name"
+            name="Family"
+          ></input>
+          <input
+            className="un"
+            type="password"
+            placeholder="Password"
+            name="Password"
+          ></input>
+          <button className="submit">Login</button>
+          <p id="badmsg"></p>
+          <button className="submit">Register</button>
+        </form>
+      </p>
+    </div>
+  );
+};
 
-                }
-                else{
-                document.getElementById("badmsg").innerText="User Not Exist's";
-                }
-            })
-    }
-
-    return(
-        <div><p>
-            <form onSubmit={loginHandler}>
-            <input type="text" placeholder="name" name="Name" ></input>
-            <input type="text" placeholder="family" name="Family" ></input>
-            <input type="password" placeholder="password" name="Password" ></input>
-            <button>Login</button>
-            </form>
-            </p>
-
-            <p id="badmsg"></p>
-
-        </div>
-    )
-}
-
-export default Login
+export default Login;
