c90da4eba1d7c5d48c1523e30ed38fcce6a08c82
Nivin <nivin.abbas1@gmail.com>
2020-08-19 09:05:35 +0300

    update task details page

--- family/src/view/components/taskDetails/TaskDetails.js
+++ family/src/view/components/taskDetails/TaskDetails.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react'
-import './TaskDetails.css'
-import Task from '../Task/Task'
+import React, { useState } from "react";
+import "./TaskDetails.css";
+import Task from "../Task/Task";
 import { useHistory } from "react-router-dom";
 //taskdetails
-const TaskDetails = (props)=>{
-    const {user} = props
-    return (
-        <div>
-            {
-                console.log(user)
-            }
-        </div>
-    )
-}
+const TaskDetails = (props) => {
+  const { user } = props;
+  return <div>{localStorage.getItem("taskContent")}</div>;
+};
 
-export default TaskDetails
+export default TaskDetails;
